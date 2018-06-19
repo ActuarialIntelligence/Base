1ac3f570459cfc35606c65d7bf1d3c50cccf4386
AccidentalCitizen <rajah.kats@gmail.com>
2018-06-19 13:01:45 +0200

    Added architecturally necessary objects and comments associated.

---
+++ src/ActuarialIntelligence.Domain/NeuralLearners/NeuralLearner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActuarialIntelligence.Domain.NeuralLearners
+{
+    /// <summary>
+    /// Learners store leaned implementations such as sum etc...
+    /// these are to be called via delegate and to be used by the processor.
+    /// This will interact with the storage environment objects to store data 
+    /// to be used for processing.
+    /// </summary>
+    public class NeuralLearner
+    {
+
+    }
+}
---
+++ src/ActuarialIntelligence.Domain/NeuralMemmories/NeuralMemmory.cs
@@ -0,0 +1,12 @@
+﻿namespace ActuarialIntelligence.Domain.NeuralMemmories
+{
+    /// <summary>
+    /// Memmory object to store Neural Memmory.
+    /// For use by learner and processor objects.
+    /// Sums of object properties for instance will be stored here if necessary.
+    /// </summary>
+    public class NeuralMemmory
+    {
+
+    }
+}
