0643b526baadadbabda7d0eb35dd5b55065add79
Rajah <rajiyer@microsoft.com>
2021-04-10 02:07:58 +0200

    added missing references.

--- src/ActuarialIntelligence.Domain/ConnectedInstruction/ConnectedInstruction.cs
+++ src/ActuarialIntelligence.Domain/ConnectedInstruction/ConnectedInstruction.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
