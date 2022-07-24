170c7ae5a938473797caf9b19eaa3d160c801f80
Rajah <rajiyer@microsoft.com>
2022-07-24 22:53:30 +0200

    Implemented Corner detection (tested and works)
    Implemented color pixcel position detection (Tested and working)

---
+++ src/ActuarialIntelligence.Infrastructure.PythonScripts/EdgeDetection.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2 as cv
+filename = 'chessboard.png'
+img = cv.imread(filename)
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+gray = np.float32(gray)
+dst = cv.cornerHarris(gray,2,3,0.04)
+#result is dilated for marking the corners, not important
+dst = cv.dilate(dst,None)
+# Threshold for an optimal value, it may vary depending on the image.
+img[dst>0.01*dst.max()]=[0,0,255]
+cv.imshow('dst',img)
+if cv.waitKey(0) & 0xff == 27:
+    cv.destroyAllWindows()
--- src/PythonRunnerEXE/Program.cs
+++ src/PythonRunnerEXE/Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+
 
 namespace PythonRunnerEXE
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            CallProcessForexecution(args);
+        }
+
+        private static void CallProcessForexecution(string[] args)
         {
             var psi = new ProcessStartInfo();
             psi.FileName = args[0];
@@ -36,6 +45,29 @@ namespace PythonRunnerEXE
                 Console.WriteLine("Results:");
                 Console.WriteLine(results);
             }
+
+          
         }
-    }
+        
+        private static IDictionary<int, int> GetPixcleList()
+        {
+
+            System.Drawing.Bitmap b = new System.Drawing.Bitmap("");
+            var colour = new Color();
+            var col = Color.FromArgb(0, 255, 0);
+            IDictionary<int,int> colorList = new Dictionary<int, int>();
+
+                for (int y = 0; y < b.Height; y++)
+                {
+                    for (int x = 0; x < b.Width; x++)
+                    {
+                        if (b.GetPixel(x, y).ToArgb().Equals(col.ToArgb()))
+                        {
+                            colorList.Add(x,y);
+                        }
+                    }
+                }
+            return colorList;
+        }
+    }  
 }
